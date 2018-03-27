63ac1e1aea5a8babd160ea3cf496b5c3b4b1f769
Vladimir Drazil <37225986+v-drazil@users.noreply.github.com>
2018-03-27 19:23:11 +0200

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -1,3 +1,4 @@
+
 # -*- coding: UTF-8 -*-
 
 """
@@ -11,6 +12,10 @@ sentences in the file and updating the database.
 
 import re
 import mysql.connector
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.parse_args()
 
 
 class FileTranslate:
@@ -26,15 +31,21 @@ class FileTranslate:
         self.txt_input = None
 
     def f_open(self):
-        self.file_input = open(file_name, 'r+', encoding='UTF-8')
+        # with open(file_name, encoding='UTF-8') as f:
+        #     self.text = f.read()
+        self.file_input = open(file_name, 'r', encoding='UTF-8')
         self.text = self.file_input.read()
+        self.file_input.close()
         self.sentences = re.split('[.?!;$]\s+', self.text)
         if '' in self.sentences:
             self.sentences.remove('')
 
     def txt_import(self):
-        self.txt_input = open(txt_file, 'r+', encoding='UTF-8')
+        # with open(txt_file, encoding='UTF-8') as f:
+        #     self.db = f.read()
+        self.txt_input = open(txt_file, 'r', encoding='UTF-8')
         self.db = self.txt_input.read()
+        self.txt_input.close()
         db_sentences = re.split('[;\n]', self.db)
         if '' in db_sentences:
             db_sentences.remove('')
@@ -59,77 +70,72 @@ class FileTranslate:
     def f_translate(self):
         if language == 'cz':
             index = 0
-            for x in self.lang1_list:
-                if x in self.text:
-                    self.text = re.sub(x, self.lang2_list[index], self.text)
-                    print('"%s" have been translated' % x)
-                    while x in self.sentences:
-                        self.sentences.remove(x)
+            for item in self.lang1_list:
+                if item in self.text:
+                    self.text = re.sub(item, self.lang2_list[index], self.text)
+                    print('"{}" have been translated' .format(item))
+                    while item in self.sentences:
+                        self.sentences.remove(item)
                 index += 1
         elif language == 'eng':
             index = 0
-            for x in self.lang2_list:
-                if x in self.text:
-                    self.text = re.sub(x, self.lang1_list[index], self.text)
-                    print('"%s" have been translated' % x)
-                    while x in self.sentences:
-                        self.sentences.remove(x)
+            for item in self.lang2_list:
+                if item in self.text:
+                    self.text = re.sub(item, self.lang1_list[index], self.text)
+                    print('"{}" have been translated' .format(item))
+                    while item in self.sentences:
+                        self.sentences.remove(item)
                 index += 1
-        self.file_input.seek(0)
+        # with open(file_name, encoding='UTF-8') as f:
+        #     f.write(self.text)
+        self.file_input = open(file_name, 'w', encoding='UTF-8')
         self.file_input.write(self.text)
+        self.file_input.close()
+
         length = len(self.sentences)
-        print('The number of untranslated sentences left in the file is: %s.'
-              % length)
+        print('The number of untranslated sentences left in the file is: {}.' .format(length))
 
     def f_manual_trans(self):
         manual_trans = input('Do you wish to translate remaining sentences manually and save them to database (y/n)? ')
         if manual_trans == 'y':
-            for y in self.sentences:
-                choice = input('Do you wish to translate "%s" and save it to database (y/n)? ' % y)
+            for item in self.sentences:
+                choice = input('Do you wish to translate "{}" and save it to database (y/n)? ' .format(item))
                 if choice == 'y':
                     new_trans = input('Input text for translation: ')
-                    self.text = re.sub(y, new_trans, self.text)
-                    print('"%s" have been translated and saved.' % y)
+                    self.text = re.sub(item, new_trans, self.text)
+                    print('"{}" have been translated and saved.' .format(item))
                     if database_choice(database_file):
                         ex = ''
                         data_ex = ''
                         if language == 'eng':
                             ex = "INSERT INTO dictionary (cz, eng) VALUES (%s, %s)"
-                            data_ex = (new_trans, y)
+                            data_ex = (new_trans, item)
                         elif language == 'cz':
                             ex = "INSERT INTO dictionary (cz, eng) VALUES (%s, %s)"
-                            data_ex = (y, new_trans)
+                            data_ex = (item, new_trans)
                         self.cursor.execute(ex, data_ex)
                         self.db.commit()
                     else:
                         txt_ex_list = []
                         if language == 'eng':
                             txt_ex_list.append(new_trans)
-                            txt_ex_list.append(y)
-                            txt_ex = ('\n' + (';'.join(txt_ex_list)))
+                            txt_ex_list.append(item)
+                            txt_ex = (';'.join(txt_ex_list)+'\n')
+                            self.txt_input = open(txt_file, 'a', encoding='UTF-8')
                             self.txt_input.write(txt_ex)
+                            self.txt_input.close()
                         elif language == 'cz':
-                            txt_ex_list.append(y)
+                            txt_ex_list.append(item)
                             txt_ex_list.append(new_trans)
-                            txt_ex = ('\n'+(';'.join(txt_ex_list)))
+                            txt_ex = (';'.join(txt_ex_list)+'\n')
+                            self.txt_input = open(txt_file, 'a', encoding='UTF-8')
                             self.txt_input.write(txt_ex)
+                            self.txt_input.close()
                 else:
                     continue
-            self.file_input.seek(0)
+            self.file_input = open(file_name, 'w', encoding='UTF-8')
             self.file_input.write(self.text)
-
-    def f_close(self):
-        # self.file_input.seek(0)
-        # self.file_input.write(self.text)
-        if database_choice(database_file):
-            self.cursor.close()
-            self.db.close()
-        else:
-            self.txt_input.close()
-        self.file_input.close()
-
-        '#user can check executed changes and then terminate the program manually.'
-        input('For termination press Enter.')
+            self.file_input.close()
 
 
 def database_choice(db_choice):
@@ -183,4 +189,10 @@ else:
 final.f_open()
 final.f_translate()
 final.f_manual_trans()
-final.f_close()
+
+if database_choice(database_file):
+    self.cursor.close()
+    self.db.close()
+
+'#user can check executed changes and then terminate the program manually.'
+input('For termination press Enter.')
